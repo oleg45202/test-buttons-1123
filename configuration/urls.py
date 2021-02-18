1b8be41d91b8462010567d9eaf22b9a2bff3dd54
oleg <gusevoleg96@gmail.ru>
2021-02-18 16:04:42 +0300

    reverted

--- configuration/urls.py
+++ configuration/urls.py
@@ -1,8 +1,8 @@
+
 from django.urls import path
 from django.conf.urls import url
 from django.views.static import serve
 from django.conf import settings
-
 from core import views
 
 urlpatterns = [
